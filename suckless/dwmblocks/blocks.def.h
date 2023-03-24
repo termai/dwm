96190f8e9f9aa0d91514677a4ecee07415204d80
termi <termi@pop-os.localdomain>
2023-03-23 20:21:42 -0500

    Fixed cache problem in folder

---
+++ suckless/dwmblocks/blocks.def.h
@@ -0,0 +1,13 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+  /*Icon*/  /*Command*/   /*Update Interval*/ /*Update Signal*/
+  {"", "prayer",  30,   0},
+  {"", "newdwmstatusbar", 30,   0},
+  {"", "batt", 1,   0},
+  {"", "~/.myconfig/dwm-blocks/time", 1,   0},
+
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
